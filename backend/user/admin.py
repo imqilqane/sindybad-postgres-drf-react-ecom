3428a79a3bfca618c8e38901f844af8b0555c03d
adam imqilqane <imqi.forum@gmail.com>
2021-10-27 12:50:29 +0100

    create the custom user model and register an admin user

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import display
+from .models import User
 
-# Register your models here.
+
+class UserManager(admin.ModelAdmin):
+    list_display = [
+        "username",
+        "email",
+        "is_superuser",
+        "is_verify"
+    ]
+
+
+admin.site.register(User, UserManager)
--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, UserManager, PermissionsMixin
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your models here.
+
+class UserManager(UserManager):
+    def create_user(self, username, email, password=None):
+        if not username:
+            raise ValueError('user has to have a username!')
+        if not email:
+            raise ValueError('user has to have an email')
+
+        user = self.model(email=self.normalize_email(email), username=username)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.is_admin = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(
+        max_length=25, unique=True, verbose_name="username")
+    email = models.EmailField(
+        max_length=255, unique=True, verbose_name="email address")
+    first_name = models.CharField(
+        max_length=25, verbose_name="first name", blank=False, null=False)
+    last_name = models.CharField(
+        max_length=25, verbose_name="last name", blank=False, null=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_verify = models.BooleanField(default=False)
+    date_joined = models.DateField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', ]
+
+    def has_perm(self, perm):
+        return True
+
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            'access': str(refresh),
+            'refresh': str(refresh.access_token)}
